7a51454327a67a87b177a733141af14a6ae8961e
Wesley Whitney <wesleywhitney@gmail.com>
2016-06-28 22:39:53 -0600

    added a storage variable for local storage using node persist

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log('starting password manager');
+
+var storage = require('node-persist');
+storage.initSync();
+
+//storage.setItemSync('accounts', [{
+    //username: 'Wesley',
+    //balance: 0
+//}]);
+
+var accounts= storage.getItemSync('accounts');
+
+    // push on a new account
+accounts.push({username: 'Whitney', balance: 100});
+    // save using setItemSync
+
+storage.setItemSync('accounts', accounts);
+
+console.log(accounts);
